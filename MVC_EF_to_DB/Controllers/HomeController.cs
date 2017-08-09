d7a5fb2d209cfb240bf49257d2846e95c3d211bf
Maryan Stoykov <stojkovmarjan@gmail.com>
2017-08-09 23:00:13 +0200

    Add project files.

---
+++ MVC_EF_to_DB/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_EF_to_DB.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*
+         * https://www.aspsnippets.com/Articles/ASPNet-MVC-Simple-Entity-Framework-Tutorial-with-example.aspx
+         * Database to EF tutorial
+         * */
+        public ActionResult Index()
+        {
+            NORTHWNDEntities entities = new NORTHWNDEntities();
+            return View(from customer in entities.Customers.Take(10)
+                        select customer);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC_EF_to_DB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_EF_to_DB.MvcApplication" Language="C#" %>
